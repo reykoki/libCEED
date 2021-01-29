e8853402405215b64f3ad043ee710971bd2290db
ArashMehraban <arashm81@gmail.com>
2021-01-28 18:33:36 -0700

    updated AppCtx

--- examples/solids/elasticity.h
+++ examples/solids/elasticity.h
@@ -124,7 +124,7 @@ struct AppCtx_private {
   PetscInt      numIncrements;                        // Number of steps
   PetscInt      bcClampCount;
   PetscInt      bcClampFaces[16];
-  PetscScalar   bcClampMax[16][7];
+  PetscScalar   bcClampMax[16][11];
   PetscInt      bcTractionCount;
   PetscInt      bcTractionFaces[16];
   PetscScalar   bcTractionVector[16][3];
